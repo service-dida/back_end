cb7a704276ecb0354144d6c820048babd8491276
JuneYoungDo <ehwnsdud1004@naver.com>
2023-05-08 21:15:23 +0900

    init

---
+++ src/main/java/com/service/dida/DidaApplication.java
@@ -0,0 +1,13 @@
+package com.service.dida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DidaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/dida/DidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.dida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
