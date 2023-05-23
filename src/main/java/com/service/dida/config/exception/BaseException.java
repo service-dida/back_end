5462b4377e92c1e05cef9a41526b5c2288426a6b
bora <youonl5@naver.com>
2023-05-23 19:31:18 +0900

    Feat: exception 처리를 위한 config 디렉토리 추가 #78

---
+++ src/main/java/com/service/dida/config/exception/BaseException.java
@@ -0,0 +1,15 @@
+package com.service.dida.config.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+
+    private final String errorCode;
+    private final String message;
+
+    public BaseException(ErrorCode code) {
+        errorCode = code.getErrorCode();
+        message = code.getMessage();
+    }
+}
---
+++ src/main/java/com/service/dida/config/exception/ErrorCode.java
@@ -0,0 +1,7 @@
+package com.service.dida.config.exception;
+
+public interface ErrorCode {
+    String getErrorCode();
+
+    String getMessage();
+}
