9964c327afe024319044e99c5da9f1a80ba015e9
JuneYoungDo <ehwnsdud1004@naver.com>
2023-05-25 01:42:16 +0900

    feat: edit error controller
    예외처리 형식 변경
    
    Resolves:
    Related to:

--- src/main/java/com/service/dida/global/config/ControllerAdvice.java
+++ src/main/java/com/service/dida/global/config/ControllerAdvice.java
@@ -1,15 +1,16 @@
-package com.service.dida.config;
+package com.service.dida.global.config;
 
-import com.service.dida.config.exception.BaseException;
-import com.service.dida.config.exception.ErrorCode;
-import com.service.dida.config.exception.errorCode.GlobalErrorCode;
-import com.service.dida.config.response.ExceptionResponse;
+import com.service.dida.global.config.exception.BaseException;
+import com.service.dida.global.config.exception.ErrorCode;
+import com.service.dida.global.config.exception.errorCode.GlobalErrorCode;
+import com.service.dida.global.config.response.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -19,58 +20,56 @@ import java.util.List;
 public class ControllerAdvice {
     @ExceptionHandler(BaseException.class)
     public ExceptionResponse handleBaseException(BaseException e) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(e.getErrorCode(),
-                e.getMessage());
-        return exceptionResponse;
+        return new ExceptionResponse(e.getErrorCode(),
+            e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ExceptionResponse handleException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ExceptionResponse> handleException(MethodArgumentNotValidException e) {
+        System.out.println(e);
         String detailMessage = extractMessage(e.getBindingResult().getFieldErrors());
         return convert(GlobalErrorCode.NOT_VALID_ARGUMENT_ERROR, detailMessage);
     }
 
     private String extractMessage(List<FieldError> fieldErrors) {
         StringBuilder builder = new StringBuilder();
-        fieldErrors.forEach((error) -> {
-            builder.append("[");
-            builder.append(error.getField());
-            builder.append(": ");
-            builder.append(error.getDefaultMessage());
-            builder.append(" || input value: ");
-            builder.append("]");
-            builder.append(System.lineSeparator());
-        });
+        fieldErrors.forEach((error) -> builder.append(error.getDefaultMessage()));
         return builder.toString();
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ExceptionResponse handleNoHandlerFoundException() {
+    public ExceptionResponse handleNoHandlerFoundException(NoHandlerFoundException e) {
+        System.out.println(e);
         return convert(GlobalErrorCode.NOT_SUPPORTED_URI_ERROR);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ExceptionResponse handleMethodNotSupportedException() {
+    public ExceptionResponse handleMethodNotSupportedException(
+        HttpRequestMethodNotSupportedException e) {
+        System.out.println(e);
         return convert(GlobalErrorCode.NOT_SUPPORTED_METHOD_ERROR);
     }
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ExceptionResponse handleMediaTypeNotSupportedException() {
+    public ExceptionResponse handleMediaTypeNotSupportedException(
+        HttpMediaTypeNotSupportedException e) {
+        System.out.println(e);
         return convert(GlobalErrorCode.NOT_SUPPORTED_MEDIA_TYPE_ERROR);
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ExceptionResponse handleRuntimeException() {
+    public ExceptionResponse handleRuntimeException(RuntimeException e) {
+        System.out.println(e);
         return convert(GlobalErrorCode.SERVER_ERROR);
     }
 
     private ExceptionResponse convert(ErrorCode e) {
-        ExceptionResponse exceptionRes = new ExceptionResponse(e.getErrorCode(), e.getMessage());
-        return exceptionRes;
+        return new ExceptionResponse(e.getErrorCode(), e.getMessage());
     }
 
-    private ExceptionResponse convert(ErrorCode e, String detailMessage) {
+    private ResponseEntity<ExceptionResponse> convert(ErrorCode e, String detailMessage) {
         ExceptionResponse exceptionRes = new ExceptionResponse(e.getErrorCode(), detailMessage);
-        return exceptionRes;
+        return new ResponseEntity<>(
+            exceptionRes, HttpStatus.BAD_REQUEST);
     }
 }
