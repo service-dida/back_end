4b2c52c2aa6b462f9ce0a51fc4d132b31d8a8dc2
JuneYoungDo <ehwnsdud1004@naver.com>
2023-05-28 04:09:47 +0900

    refactor: Transaction Type 추가
    스왑 별로 Transaction Type 분리
    
    Resolves:
    Related to:

--- src/main/java/com/service/dida/domain/transaction/Type.java
+++ src/main/java/com/service/dida/domain/transaction/Type.java
@@ -2,7 +2,8 @@ package com.service.dida.domain.transaction;
 
 public enum Type {
     MINTING("NFT 발행"),
-    SWAP("스왑"),
+    SWAP1("클레이를 디다로 스왑"),
+    SWAP2("디다를 클레이로 스왑"),
     DEAL("거래");
 
     private final String name;
