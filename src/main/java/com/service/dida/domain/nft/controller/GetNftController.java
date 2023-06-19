0490e2d17399f3fae894a0022a1beaf9e2c09ec6
JuneYoungDo <ehwnsdud1004@naver.com>
2023-06-19 19:13:59 +0900

    feat: NFT 상세보기 Api
    NFT 상세보기 Api 개발
    
    Resolves: #37
    Related to:

---
+++ src/main/java/com/service/dida/domain/nft/controller/GetNftController.java
@@ -0,0 +1,27 @@
+package com.service.dida.domain.nft.controller;
+
+import com.service.dida.domain.member.entity.Member;
+import com.service.dida.domain.nft.dto.NftResponseDto.NftDetailInfo;
+import com.service.dida.domain.nft.usecase.GetNftUseCase;
+import com.service.dida.global.config.security.auth.CurrentMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GetNftController {
+    private final GetNftUseCase getNftUseCase;
+
+    /**
+     * NFT 상세 보기 Api
+     */
+    @GetMapping("/common/nft/{nftId}")
+    public ResponseEntity<NftDetailInfo> getNftDetail(@CurrentMember Member member,
+        @PathVariable(value = "nftId") Long nftId) {
+        return new ResponseEntity<>(getNftUseCase.getNftDetail(member, nftId), HttpStatus.OK);
+    }
+}
--- src/main/java/com/service/dida/domain/nft/dto/NftResponseDto.java
+++ src/main/java/com/service/dida/domain/nft/dto/NftResponseDto.java
@@ -1,5 +1,6 @@
 package com.service.dida.domain.nft.dto;
 
+import com.service.dida.domain.member.dto.MemberResponseDto.MemberInfo;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,5 +15,18 @@ public class NftResponseDto {
         private String nftName;
         private String nftImgUrl;
         private String price;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    public static class NftDetailInfo {
+        private NftInfo nftInfo;
+        private String description;
+        private MemberInfo memberInfo;
+        private String tokenId;
+        private String contractAddress;
+        private boolean followed;
+        private boolean liked;
     }
 }
