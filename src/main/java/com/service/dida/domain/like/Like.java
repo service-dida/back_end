65e51dde9d9fe6e6c8e92705717669e89ce2a61d
JuneYoungDo <ehwnsdud1004@naver.com>
2023-06-19 19:10:41 +0900

    refactor: * 표시 삭제
    * 표시로 되어있던 부분 수정
    
    Resolves:
    Related to:

--- src/main/java/com/service/dida/domain/like/Like.java
+++ src/main/java/com/service/dida/domain/like/Like.java
@@ -1,9 +1,20 @@
 package com.service.dida.domain.like;
 
-import com.service.dida.domain.nft.Nft;
 import com.service.dida.domain.member.entity.Member;
-import jakarta.persistence.*;
-import lombok.*;
+import com.service.dida.domain.nft.Nft;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
