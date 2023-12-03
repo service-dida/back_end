a9494e83568ce0a532a6e6366a077f14879ff5f8
bora <youonl5@naver.com>
2023-12-03 16:28:09 +0900

    Feat: LogAspect printLog 구현
    모든 Controller, Service 방문 시 로그 출력

---
+++ src/main/java/com/service/dida/global/config/log/LogAspect.java
@@ -0,0 +1,28 @@
+package com.service.dida.global.config.log;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.util.StringUtils;
+
+@Aspect
+@Log4j2
+@Component
+public class LogAspect {
+
+    @Around("execution(* com.service.dida.domain..*Controller.*(..)) || execution(* com.service.dida.domain..*Service.*(..))")
+    public Object printLog(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        String name = joinPoint.getSignature().getDeclaringTypeName();
+        String type =
+                StringUtils.contains(name, "Controller") ? "Controller ===> " :
+                StringUtils.contains(name, "Service") ? "Service ===> " :
+                "";
+
+        log.info(type + name + "." + joinPoint.getSignature().getName() + "()");
+        return joinPoint.proceed();
+    }
+
+}
