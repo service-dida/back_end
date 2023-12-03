0a55c8e81effe61708a09064662ce6f4dd8675cd
bora <youonl5@naver.com>
2023-12-03 16:26:39 +0900

    Feat: WebConfig 설정을 통해 Interceptor 활성화

---
+++ src/main/java/com/service/dida/global/config/log/WebConfig.java
@@ -0,0 +1,19 @@
+package com.service.dida.global.config.log;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final Interceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**");    // 모든 Request 에 대해
+    }
+}
