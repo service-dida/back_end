25c6df8b532fd40817b05ee455185957462e5d96
bora <youonl5@naver.com>
2023-12-03 16:35:40 +0900

    Feat: BaseException 발생 시 Error Log 찍기

--- src/main/java/com/service/dida/global/config/exception/BaseException.java
+++ src/main/java/com/service/dida/global/config/exception/BaseException.java
@@ -1,8 +1,10 @@
 package com.service.dida.global.config.exception;
 
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 
+@Log4j2
 @Getter
 public class BaseException extends RuntimeException {
 
@@ -14,6 +16,7 @@ public class BaseException extends RuntimeException {
         errorCode = code.getErrorCode();
         message = code.getMessage();
         status = code.getStatus();
+        log.error("In BaseException ErrorCode: {}, ErrorMessage: {}, Status: {}", errorCode, message, status);
     }
 
     public void setAiErrorMessage(String message) {
